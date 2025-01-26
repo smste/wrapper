abe676b7a6042c24a9c9f9280917aded25c499c9
Sam <samsemail112@gmail.com>
2025-01-26 15:36:00 +1100

    authentication

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // MongoDB Configurations 
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@tripscluster.ajq0e.mongodb.net/playerData?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@tripscluster.ajq0e.mongodb.net/playerData?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true, tlsAllowInvalidCertificates: true });
 
 var db = mongoose.connection;
 
@@ -17,6 +17,28 @@ db.once("open", function() {
 
 require('./models/player')
 const playerModel = mongoose.model('Player')
+
+app.use((req, res, next) => {
+
+  // authentication middleware
+
+  const auth = {login: `${process.env.AUTH_LOGIN}`, password: `${process.env.AUTH_PWD}`} // change this
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next()
+  }
+
+  // Access denied lol...
+  res.set('WWW-Authenticate', 'Basic realm="401"') // change this if you want to be a 
+  res.status(401).send('Authentication required.') // custom message
+
+})
 
 app.get("/", (request, response) => {
   response.send("No Access");
